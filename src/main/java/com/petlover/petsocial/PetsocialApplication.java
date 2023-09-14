8275ee8043d3293d35447a30a341fb75cf15c522
huynhlephcvinh <phucvinh710@gmail.com>
2023-09-14 09:33:01 +0700

    Add Main

---
+++ src/main/java/com/petlover/petsocial/PetsocialApplication.java
@@ -0,0 +1,13 @@
+package com.petlover.petsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsocialApplication.class, args);
+	}
+
+}
